fa60cf736ae44e497dcc45d29cfaaad6a8d528f4
Fernando Cardozo <fernando_cardozo@poli.ufrj.br>
2022-03-28 21:21:07 -0300

    update: refactor for Typescript and changed class components to function components

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,59 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { FormHandles } from '@unform/core';
+
+interface IEditFood {
+	image: string,
+	price: string,
+	description: string,
+	name: string
+}
+
+interface IFood extends IEditFood {
+	id: number,
+	available: boolean
+}
+
+interface ModalEditFoodProps {
+	isOpen: boolean,
+	handleUpdateFood(data: IEditFood): void,
+	setIsOpen(): void,
+	editingFood: IFood
+}
+
+function ModalEditFood({ isOpen, handleUpdateFood, setIsOpen, editingFood }: ModalEditFoodProps): JSX.Element {
+
+	const formRef = useRef<FormHandles>(null)
+
+	async function handleSubmit(data: IEditFood) {
+		handleUpdateFood(data)
+		setIsOpen()
+	}
+
+	return (
+		<Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+			<Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+				<h1>Editar Prato</h1>
+				<Input name="image" placeholder="Cole o link aqui" />
+
+				<Input name="name" placeholder="Ex: Moda Italiana" />
+				<Input name="price" placeholder="Ex: 19.90" />
+
+				<Input name="description" placeholder="Descrição" />
+
+				<button type="submit" data-testid="edit-food-button">
+					<div className="text">Editar Prato</div>
+					<div className="icon">
+						<FiCheckSquare size={24} />
+					</div>
+				</button>
+			</Form>
+		</Modal>
+	)
+}
+
+export default ModalEditFood;
